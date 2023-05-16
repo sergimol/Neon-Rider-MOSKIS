610a6c30c14473700405b8bcf457a938ab13d896
Sergio Molinero <sergimol@ucm.es>
2023-05-16 18:44:24 +0200

    Juego

---
+++ Assets/Scripts/Scenario/Checkpoint.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public int deadVal = 0;
+    public bool isLast = false;
+    public bool isFirst = false;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerController>() != null)
+        {
+            // Transforma ésta en la posición de respawn del jugador
+            // y se encarga de evitar que revivan los enemigos anteriores
+            GameManager.instance.checkpoint = transform.position;
+            if (deadVal > GameManager.instance.deadVal)
+            {
+                if (!isFirst)
+                    //Tracker.instance.AddEvent(new FinSalaEvent(deadVal));
+                if (!isLast)
+                    //Tracker.instance.AddEvent(new InicioSalaEvent(deadVal+1));
+                GameManager.instance.deadVal = deadVal;
+            }
+        }
+    }
+}
